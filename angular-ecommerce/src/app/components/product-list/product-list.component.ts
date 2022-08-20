6439459e5e948ab69f401887c597a6583a5a0cc1
Rodney <rodney.mapako@synthesis.com>
2022-08-20 14:41:18 +0200

    Added Pagination and PagSize

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -17,8 +17,10 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
+
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -46,12 +48,21 @@ export class ProductListComponent implements OnInit {
 
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
+    //if we have a differnt keyword than previous
+    //then set thePageNumber to 1
+
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;    
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`)
+
     // now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                               this.thePageSize,
+                                               theKeyword ).subscribe(this.processResult())
   }
 
   handleListProducts() {
@@ -90,8 +101,14 @@ export class ProductListComponent implements OnInit {
                                                .subscribe(this.processResult());
   }
 
+  updatePageSize(pageSize: string) {
+    this.thePageSize= +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
   processResult() {
-    return data => {
+    return (data: any) => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
